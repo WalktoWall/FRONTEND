d62d7a31a1313c10ee330b875eadbf73ca857a48
seobin0606 <sobenio2@naver.com>
2026-08-20 02:07:39 +0900

    fix QR css import casing

--- src/pages/QR/jsx/Qr.jsx
+++ src/pages/QR/jsx/Qr.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/QR.css";
+import "../css/qr.css";
 
 import BackBtn from "../../../components/jsx/BackBtn";
 import BottomNav from "../../../components/jsx/BottomNav";
